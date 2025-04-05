c62fbb59b90d1859df4b1781df1725951ddee621
Juan Carlos <jcarlosjccm@gmail.com>
2025-04-05 23:17:49 +0200

    Incluimos fullname en el login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -53,7 +53,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true, id: true }
+      select: { email: true, password: true, id: true, fullName: true, isActive: true, roles: true }
     });
 
     if (!user) {
